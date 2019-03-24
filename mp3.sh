3cabb7ab5a015886a70c8e6450d5103e0f4db639
iasatan <satanadam@outlook.com>
2019-03-24 16:29:50 +0100

    mp3.sh updated

--- mp3.sh
+++ mp3.sh
@@ -4,6 +4,7 @@ bash "$basedir"/removeBad.sh
 echo "removeDuplicates-------------------------------------------------------------------------"
 bash "$basedir"/removeDuplicates.sh "$basedir"
 echo "commiting to git"
+cd "$basedir/MP3Library"
 #git pull
 git add .
 git commit -m "$directory"" added"
