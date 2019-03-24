d1c5deebbd025e32e018d47bd0b5269a6e5c0715
iasatan <satanadam@outlook.com>
2019-03-24 13:02:32 +0100

    mp3.sh rolled back

--- removeBad.sh
+++ removeBad.sh
@@ -2,17 +2,17 @@
 basedir="/home/iasatan/Downloads/MP3Filter";
 touch /tmp/tempBadArtists.txt
 echo "removeNonMusic-------------------------------------------------------------------------"
-#bash "$basedir"/removeNonMusic.sh
+bash "$basedir"/removeNonMusic.sh
 echo "remove unoriginal music-------------------------------------------------------------------------"
-#bash "$basedir"/removeUnoriginalMusic.sh
+bash "$basedir"/removeUnoriginalMusic.sh
 echo "removeArtists-------------------------------------------------------------------------"
-#bash "$basedir"/removeArtists2.sh "$basedir/MP3Library"/artists.txt
+bash "$basedir"/removeArtists2.sh "$basedir/MP3Library"/artists.txt
 echo "removeShortOrLongMusic-------------------------------------------------------------------------"
-#bash "$basedir"/removeShortOrLongSongs.sh
+bash "$basedir"/removeShortOrLongSongs.sh
 echo "removeArtists2-------------------------------------------------------------------------"
-#bash "$basedir"/removeArtists.sh "$basedir"
+bash "$basedir"/removeArtists.sh "$basedir"
 echo "Remove Bad Genres-------------------------------------------------------------------------"
-#bash "$basedir"/removeBadGenres.sh "$basedir"
+bash "$basedir"/removeBadGenres.sh "$basedir"
 echo "classify new artists"
 bash "$basedir"/checkArtist.sh "$basedir"
 echo "removeArtists-------------------------------------------------------------------------"
